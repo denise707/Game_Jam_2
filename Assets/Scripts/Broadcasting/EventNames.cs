c9ff22141999cec15957921fe06ae3c8424ab043
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2021-09-07 00:15:05 +0800

    player hp ui + script

--- Assets/Scripts/Broadcasting/EventNames.cs
+++ Assets/Scripts/Broadcasting/EventNames.cs
@@ -8,15 +8,12 @@ using System.Collections;
 public class EventNames {
 	public const string ON_PLAYER_LOSE = "ON_PLAYER_LOSE";
 	public const string ON_PLAYER_WIN = "ON_PLAYER_WIN";
-	public const string ON_PLAYER_FREEZING = "ON_PLAYER_FREEZING";
-	public const string ON_NOTE_FOUND = "ON_NOTE_FOUND";
-	public const string ON_ITEM_PICKUP = "ON_NOTE_FOUND";
-	public const string ON_ITEM_LIGHTED = "ON_ITEM_LIGHTED";
 	public const string ON_EAT_FOOD = "ON_NOTE_FOUND";
 	public const string ON_TAKE_BATH = "ON_TAKE_BATH";
 	public const string ON_GO_TO_SLEEP = "ON_GO_TO_SLEEP";
 	public const string ON_DAY_OVER = "ON_DAY_OVER";
 	public const string ON_GAME_START = "ON_GAME_START";
+	public const string ON_TASKS_FAILED = "ON_TASKS_FAILED";
 }
 
 
